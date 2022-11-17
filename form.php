ad290fba9835d25b86dc74f09dc5da3ee3d7e529
Fer3Orenday <fer.orenday3@gmail.com>
2022-11-17 09:06:15 -0600

    Agregue el body

--- form.php
+++ form.php
@@ -1,12 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <h2>María Fernanda Hermosillo Orenday</h2>
+
+    <form action="#" method="post">
+        <table>
+            <tr>
+                <td>Total de renglones:</td>
+                <td><input type="number" name="x"></td>
+            </tr>
+            <tr>
+                <td>Limite inferior:</td>
+                <td><input type="number" name="c" required></td>
+            </tr>
+            <tr>
+                <td><input type="number" name="d" required></td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align:center"><input type="submit" name="send" value="enviar"></td>
+            </tr>
+        </table>
+    </form>
+    <br>
 </body>
+
 </html>
